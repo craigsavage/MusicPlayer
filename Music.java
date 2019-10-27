5ab01b8f337d135c6ba34b4019186c5cb58b5918
Craig Savage <31861025+craigsavage@users.noreply.github.com>
2019-10-27 08:06:12 -0400

    Update Music.java
    Started implementing functionality for downloading songs to the folder

--- Music.java
+++ Music.java
@@ -10,12 +10,19 @@ public class Music {
         System.out.print("Which song would you like to play?\n--> ");
 
         // Contains name of the user's selected song
-        String selectedSong = in.nextLine().toLowerCase().trim();
-
-                
+        String selectedSong = in.nextLine().toLowerCase().trim();  
 
         return selectedSong;
     }
+
+    public static void downloadSong(String song) {
+		
+		// The name of the folder to download song files to 
+		String downloadLocation = "\\MusicDownloadedFromServer\\";
+
+		// Inform user that their specific song is being downloaded to the folder
+		System.out.printf("Downloading \"%s.mp3\" to \"%s\" folder.\n", song, downloadLocation);
+	}
 
     public static void main(String args[]) {
         
